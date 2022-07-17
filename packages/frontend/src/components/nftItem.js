53de47820315c586d716680474170f95719210e4
Diego Alzate <diego35.da@gmail.com>
2022-07-17 18:31:16 -0500

    feat: nft card style for json

--- packages/frontend/src/components/nftItem.js
+++ packages/frontend/src/components/nftItem.js
@@ -2,19 +2,17 @@ import react from "react";
 
 const NftItem = ({nft, buttonTitle, buttonFunc}) => {
   return (
-    <div  className="border shadow rounded-xl overflow-hidden">
-      <img src={nft.image} width={300}/>
-      <div className="p-4">
+    <div  className="border shadow-md rounded-xl overflow-hidden">
+      <img src={nft.image}/>
+      <div className="px-4">
         <p style={{ height: '64px' }} className="text-2xl font-semibold">{nft.name}</p>
         <div style={{ height: '70px', overflow: 'hidden' }}>
           <p className="text-gray-400">{nft.description}</p>
         </div>
-        {JSON.stringify(nft, null, '\n')}
+        <p className="bg-slate-700 text-green-500 rounded-lg p-4 overflow-scroll">
+          {JSON.stringify(nft, null, '\n')}
+        </p>
       </div>
-      {/* <div className="p-4 bg-black">
-        <p className="text-2xl font-bold text-white">{nft.donationBalance} ETH</p>
-        <button className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded" onClick={buttonFunc}>{buttonTitle}</button>
-      </div> */}
     </div>
   )
 }
